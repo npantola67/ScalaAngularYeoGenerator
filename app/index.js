de4e453034b6819afafe4c971365b3f3405efd91
NickPantola <Npantola67@gmail.com>
2014-06-22 14:24:23 -0500

    Skeleton index.js

--- app/index.js
+++ app/index.js
@@ -7,48 +7,7 @@ var chalk = require('chalk');
 
 
 var ScalaangularGenerator = yeoman.generators.Base.extend({
-  init: function () {
-    this.pkg = require('../package.json');
 
-    this.on('end', function () {
-      if (!this.options['skip-install']) {
-        this.installDependencies();
-      }
-    });
-  },
-
-  askFor: function () {
-    var done = this.async();
-
-    // Have Yeoman greet the user.
-    this.log(yosay('Welcome to the marvelous Scalaangular generator!'));
-
-    var prompts = [{
-      type: 'confirm',
-      name: 'someOption',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
-
-    this.prompt(prompts, function (props) {
-      this.someOption = props.someOption;
-
-      done();
-    }.bind(this));
-  },
-
-  app: function () {
-    this.mkdir('app');
-    this.mkdir('app/templates');
-
-    this.copy('_package.json', 'package.json');
-    this.copy('_bower.json', 'bower.json');
-  },
-
-  projectfiles: function () {
-    this.copy('editorconfig', '.editorconfig');
-    this.copy('jshintrc', '.jshintrc');
-  }
 });
 
 module.exports = ScalaangularGenerator;
